6619018ea0ee32756b50c93b113401129e49e3c8
fersp98 <fernando.socualaya@utec.edu.pe>
2018-09-02 20:05:30 -0500

    Se implementaros nuevas características. pop_front y pop_back aún en desarrollo

--- list.h
+++ list.h
@@ -13,23 +13,103 @@ class List {
         Node<T>* start;
 
     public:
-        List();
+        List(){
+            start = nullptr;
+        };
 
-        T front();
-        T back();
-        void push_front(T value);
-        void push_back(T value);
-        void pop_front();
-        void pop_back();
-        T get(int position);
-        void concat(List<T> &other);
-        bool empty();
-        int size();
-        void clear();
+        T front(){
+            return start->data;
+        };
+
+        T back(){
+            return start->prev->data;
+        };
+
+        void push_front(T value){
+            auto* node = new Node<T>;
+            node->data = value;
+            if(start){
+                node->next =this->start;
+                node->prev = this->start->prev;
+                this->start->prev->next = node;
+                this->start->prev = node;
+                this->start = node;
+                cout<<"Pushed in non empty list\n";
+            }
+            else{
+                node->prev = node;
+                node->next = node;
+                this->start = node;
+                cout<<"Pushed in empty list\n";
+            }
+        };
+
+        void push_back(T value){
+            auto node = new Node<T>;
+            node->data = value;
+            if(start){
+                node->next = this->start;
+                node->prev = this->start->prev->next;
+                this->start->prev->next = node;
+                this->start->prev = node;
+                cout<<"Pushed in non empty list\n";
+            }
+            else{
+                node->prev = node;
+                node->next = node;
+                this->start = node;
+                cout<<"Pushed in empty list\n";
+            }
+        };
+        void pop_front(){
+            if(start){
+                auto* tmp = new Node<T>;
+                tmp = this->start;
+                this->start->prev->next = this->start->next;
+                this->start->next->prev = this->start->prev;
+                this->start = this->start->next;
+                if(this->start == this->start->next)
+                    cout<<"One single element in list!\n";
+                delete tmp;
+            }
+            else{
+                cout<<"Error: Empty List!\n";
+            }
+        };
+        void pop_back(){
+
+        };
+
+        T get(int position){
+
+        };
+
+        void concat(List<T> &other){
+            this->start->prev->next = other.start;
+        };
+
+        bool empty(){
+            return (bool)!(this->start);
+        };
+
+        int size(){
+            int n=1;
+            auto* temp = this->start;
+            while(temp->next!=start){
+                temp = temp->next;
+                n++;
+            }
+            return n;
+        };
+
+        void clear(){
+
+        };
+
         Iterator<T> begin();
         Iterator<T> end();
 
-        ~List();
+        ~List()= default;
 };
 
 #endif
